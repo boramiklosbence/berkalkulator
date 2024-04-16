128d9624e470243660c6562d72f640e508fdea5a
borambence <boramiklosbence@gmail.com>
2024-04-17 00:07:32 +0200

    Daily

--- src/components/FamilyMemberTabs/FamilyMemberTabs.jsx
+++ src/components/FamilyMemberTabs/FamilyMemberTabs.jsx
@@ -2,27 +2,16 @@ import { FaPlus as PlusIcon } from "react-icons/fa";
 
 const FamilyMemberTabs = () => {
   return (
-    <div className="container m-2">
-      <div className="flex flex-nowrap items-center gap-x-1">
-        <button
-          type="button"
-          className="h-10 rounded bg-sky-100 px-4 py-2 font-bold text-black hover:bg-blue-600"
-        >
-          Button
-        </button>
-        <button
-          type="button"
-          className="h-10 rounded bg-sky-100 px-4 py-2 font-bold text-black hover:bg-blue-600"
-        >
-          Button
-        </button>
-        <button
-          type="button"
-          className="h-10 rounded bg-sky-100 px-4 py-2 font-bold text-black hover:bg-blue-600"
-        >
-          <PlusIcon />
-        </button>
-      </div>
+    <div className="m-2 flex flex-nowrap items-center justify-center gap-x-1">
+      <button type="button" className="h-10 rounded bg-blue-100 px-4 py-2 text-black hover:bg-blue-600">
+        Button
+      </button>
+      <button type="button" className="h-10 rounded bg-blue-100 px-4 py-2 text-black hover:bg-blue-600">
+        Button
+      </button>
+      <button type="button" className="h-10 rounded bg-blue-100 px-4 py-2 text-black hover:bg-blue-600">
+        <PlusIcon />
+      </button>
     </div>
   );
 };
--- src/components/HouseholdSummary/HouseholdSummary.jsx
+++ src/components/HouseholdSummary/HouseholdSummary.jsx
@@ -1,7 +1,35 @@
 const HouseholdSummary = () => {
   return (
-    <div className="container inline-block w-1/2 rounded bg-sky-100 p-4">
-      HouseholdSummary
+    <div className="mb-2 flex justify-center">
+      <div className="w-1/3 rounded bg-blue-100 p-4">
+        <p className="mb-2 text-lg font-bold uppercase">Háztartás összesített jövedelme</p>
+        <table className="w-full text-left text-sm text-black">
+          <thead className="bg-gray-50 text-xs uppercase">
+            <tr className="border-b">
+              <th scope="col" className="px-6 py-3">
+                Családtag
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Nettó bér
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border-b bg-white">
+              <td className="whitespace-nowrap px-6 py-4 font-medium">Apple MacBook Pro 17"</td>
+              <td className="px-6 py-4">Silver</td>
+            </tr>
+            <tr className="border-b bg-white">
+              <td className="whitespace-nowrap px-6 py-4 font-medium">Microsoft Surface Pro</td>
+              <td className="px-6 py-4">White</td>
+            </tr>
+            <tr className="bg-white">
+              <td className="whitespace-nowrap px-6 py-4 font-medium">Magic Mouse 2</td>
+              <td className="px-6 py-4">Black</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
